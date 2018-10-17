aa1096cd93b45ac1613e799c6d0db44cc1d2fb40
Kevin Tong <kcr1230@hotmail.com>
2018-10-17 01:49:01 -0700

    hi

--- src/homework-2/prefix.c
+++ src/homework-2/prefix.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <stdlib.h>
+#include <time.h>
 
 void Read_n(int *n, int *local_n, int my_rank, int comm_sz, MPI_Comm comm);
 
-void Generate_data(int vector, int local_vector, int n, int my_rank, int comm_sz, MPI_Comm comm);
+void Generate_data(int *prefix, int n, int local_n, int my_rank, MPI_Comm comm);
+
+void Prefix_sum(int *prefix, int *final_prefix_sum, int n, int local_n, int my_rank, MPI_Comm comm);
 
 int main(int argc, char **argv) {
   
@@ -12,10 +15,9 @@ int main(int argc, char **argv) {
   int comm_sz;
   int n;
   int local_n;
-  int *vector;
-  int *local_vector;
+  int local_prefix_sum;
   int *prefix;
-  int *local_prefix;
+  int *final_prefix_sum;
   MPI_Comm comm;
   
   comm = MPI_COMM_WORLD;
@@ -25,24 +27,42 @@ int main(int argc, char **argv) {
   
   // read n
   Read_n(&n, &local_n, my_rank, comm_sz, comm);
+
+  prefix = malloc(local_n * sizeof(int));
+  final_prefix_sum = malloc(local_n * sizeof(int));
   
-  vector = malloc(n * sizeof(int));
-  local_vector = malloc(n * sizeof(int));
-  prefix = malloc(n * sizeof(int));
-  local_prefix = malloc(n * sizeof(int));
+  Generate_data(prefix, n, local_n, my_rank, comm);
   
-  Generate_data(*vector, *local_vector, n, my_rank, comm_sz, comm);
-  
-  for (int i = 0; i < *vector; ++i) {
-    printf("%d\n", vector[i]);
+  if (my_rank == 0) {
+    printf("prefix array: [");
+    for (int i = 0; i < n; ++i) {
+      printf(" %d ", prefix[i]);
+    }
+    printf("]\n");
   }
   
-  MPI_Scan(prefix, local_prefix, 1, MPI_INT, MPI_SUM, comm);
+  MPI_Scan(&prefix, &local_prefix_sum, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+  MPI_Gather(&local_prefix_sum, 1, MPI_INT, final_prefix_sum, 1, MPI_INT, 0, comm);
+
+  if (my_rank == 0) {
+    printf("prefix sums");
+    for (int i = 0; i < n; i++) {
+      printf("\nsum[%d] = %d", i, final_prefix_sum[i]);
+    }
+    printf("\n");
+  }
   
+  free(prefix);
+  free(final_prefix_sum);
   MPI_Finalize();
 }
 
 void Read_n(int *n, int *local_n, int my_rank, int comm_sz, MPI_Comm comm) {
+  
+  MPI_Datatype type;
+  
+  MPI_Type_contiguous(1, MPI_INT, &type);
+  MPI_Type_commit(&type);
   
   // get input
   if (my_rank == 0) {
@@ -50,24 +70,47 @@ void Read_n(int *n, int *local_n, int my_rank, int comm_sz, MPI_Comm comm) {
     scanf("%d", n);
   }
   
-  MPI_Bcast(n, 1, MPI_INT, 0, comm);
+  MPI_Bcast(n, 1, type, 0, comm);
   
   *local_n = *n / comm_sz;
 }
 
-void Generate_data(int vector, int local_vector, int n, int my_rank, int comm_sz, MPI_Comm comm) {
-  int *data;
+void Generate_data(int *prefix, int n, int local_n, int my_rank, MPI_Comm comm) {
+  int *data = NULL;
   
   if (my_rank == 0) {
-    data = malloc(n * sizeof(int));
-    for (int i = 0; i <= n; ++i) {
-      data[i] = rand() % 10 + 1;
+    data = malloc(local_n * sizeof(int));
+    
+    srand((unsigned int) time(0));
+    
+    for (int i = 0; i < local_n; i++) {
+      data[i] = rand() % 23;
     }
   }
   
-  MPI_Scatter(data, local_vector, MPI_INT, n, my_rank, MPI_INT, 0, comm);
+  MPI_Bcast( data, 1, MPI_INT, 0, comm);
+  MPI_Scatter(data, n, MPI_INT, prefix, n, MPI_INT, 0, comm);
   
   if (my_rank == 0) {
     free(data);
+  }
+}
+
+void Prefix_sum(int *prefix, int *final_prefix_sum, int n, int local_n, int my_rank, MPI_Comm comm) {
+  int *data = NULL;
+  
+  if (my_rank == 0) {
+    data = malloc(local_n * sizeof(int));
+    prefix[0] = data[0];
+
+    for (int i = 1; i < local_n; i++) {
+      data[i] = data[i - 1] + prefix[i];
+    }
+  }
+  
+  MPI_Scatter(final_prefix_sum, n, MPI_INT, prefix, n, MPI_INT, 0, comm);
+
+  if (my_rank == 0) {
+    free(final_prefix_sum);
   }
 }
