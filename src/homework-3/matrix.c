84e225354e295d5c820edc6e73f091f54e402a00
Kevin Tong <kcr1230@hotmail.com>
2018-10-29 01:33:32 -0700

    hi

--- src/homework-3/matrix.c
+++ src/homework-3/matrix.c
@@ -1,7 +1,14 @@
+//
+// Created by Kevin Tong on 22/10/2018.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <time.h>
 #include <mpi.h>
+#include <math.h>
+
+#define TYPE MPI_DOUBLE
 
 void Read_n(int *n_p, int *local_n_p, int my_rank, int comm_sz,
             MPI_Comm comm);
@@ -14,12 +21,18 @@ void Read_data(double *local_matrix, double *local_vector, int local_n, int my_r
 void Print_vector(double local_vec[], int local_n, int n, char title[],
                   int my_rank, MPI_Comm comm);
 
-void Par_vector_addition(const double *local_matrix, const double *local_vector, double *local_result, int local_n);
+void Print_vector_2d(double local_vec[], int local_n, int n, char title[],
+                     int my_rank, MPI_Comm comm);
+
+void
+Mat_vect_mult(const double *local_a, const double *local_x, double *local_result, int local_m, int n, int local_n);
 
 int main(void) {
   int n, local_n;
-  double *local_vec1, *local_vec2;
-  double *addition_vector;
+//  double *local_matrix, *local_vector;
+  double *local_matrix;
+  double *local_vector;
+  double *final_vector;
   int comm_sz, my_rank;
   MPI_Comm comm;
   
@@ -32,32 +45,33 @@ int main(void) {
   Read_n(&n, &local_n, my_rank, comm_sz, comm);
   
   // need malloc
-  local_vec1 = malloc(local_n * sizeof(double));
-  local_vec2 = malloc(local_n * sizeof(double));
-  addition_vector = malloc(local_n * sizeof(double));
+  local_matrix = malloc(local_n * sizeof(double));
+  local_vector = malloc(local_n * sizeof(double));
+  final_vector = malloc(local_n * sizeof(double));
   
   // read vectors and scatter to processes
-  Read_data(local_vec1, local_vec2, local_n, my_rank, comm_sz, comm);
+  Read_data(local_matrix, local_vector, local_n, my_rank, comm_sz, comm);
   
   /* Print input data */
   if (my_rank == 0) {
     printf("\n––input––\n");
   }
-  Print_vector(local_vec1, local_n, n, "vector x: ", my_rank, comm);
-  Print_vector(local_vec2, local_n, n, "vector y: ", my_rank, comm);
+  
+  Print_vector_2d(local_matrix, local_n, n, "matrix: ", my_rank, comm);
+  Print_vector(local_vector, local_n, n, "vector: ", my_rank, comm);
   
   /* Print results */
   if (my_rank == 0) {
     printf("\n––output––\n");
   }
   
-  /* Compute addition and print out result */
-  Par_vector_addition(local_vec1, local_vec2, addition_vector, local_n);
-  Print_vector(addition_vector, local_n, n, "addition vector of vector x and y:", my_rank, comm);
+  /* Compute matrix-vector multiplication and print out result */
+  Mat_vect_mult(local_matrix, local_vector, final_vector, n, n, local_n);
+  Print_vector(final_vector, local_n, n, "matrix-vector multiplication result:", my_rank, comm);
   
-  free(addition_vector);
-  free(local_vec1);
-  free(local_vec2);
+  free(final_vector);
+  free(local_matrix);
+  free(local_vector);
   
   MPI_Finalize();
   return 0;
@@ -85,14 +99,13 @@ void Check_for_error(
   }
 }  /* Check_for_error */
 
-
 /*-------------------------------------------------------------------*/
 void Read_n(int *n_p, int *local_n_p, int my_rank, int comm_sz,
             MPI_Comm comm) {
   int local_ok = 1;
   
   if (my_rank == 0) {
-    printf("what is the order of your vector: ");
+    printf("what is the order of your square matrix? ");
     printf("\n");
     scanf("%d", n_p);
   }
@@ -110,37 +123,39 @@ void Read_n(int *n_p, int *local_n_p, int my_rank, int comm_sz,
   *local_n_p = *n_p / comm_sz;
 }  /* Read_n */
 
-
 /*-------------------------------------------------------------------*/
 void Read_data(double *local_matrix, double *local_vector, int local_n, int my_rank, int comm_sz, MPI_Comm comm) {
   double *a = NULL;
+  double *b = NULL;
   int i;
-  MPI_Datatype type;
   
-  MPI_Type_contiguous(1, MPI_DOUBLE, &type);
-  MPI_Type_commit(&type);
+  srand((unsigned int) time(NULL));
   
   if (my_rank == 0) {
-    a = malloc(local_n * comm_sz * sizeof(double));
+    a = malloc(local_n * local_n * comm_sz * sizeof(double));
+    b = malloc(local_n * comm_sz * sizeof(double));
     
     // first vector
-    printf("Enter your x vector\n");
-    for (i = 0; i < local_n * comm_sz; i++) {
-      scanf("%lf", &a[i]);
+    printf("Enter your matrix\n");
+    for (i = 0; i < pow(local_n, 2) * comm_sz; i++) {
+        scanf("%lf", &a[i]);
+//        a[i] = rand() % 10;
     }
     // scatter vector 1
-    MPI_Scatter(a, local_n, type, local_matrix, local_n, type, 0, comm);
+    MPI_Scatter(a, local_n, TYPE, local_matrix, local_n, TYPE, 0, comm);
     // second vector
-    printf("Enter your y vector\n");
+    printf("Enter your vector\n");
     for (i = 0; i < local_n * comm_sz; i++) {
-      scanf("%lf", &a[i]);
+      scanf("%lf", &b[i]);
+//      b[i] = rand() % 10;
     }
     // scatter vector 2
-    MPI_Scatter(a, local_n, type, local_vector, local_n, type, 0, comm);
+    MPI_Scatter(b, local_n, TYPE, local_vector, local_n, TYPE, 0, comm);
     free(a);
+    free(b);
   } else {
-    MPI_Scatter(a, local_n, type, local_matrix, local_n, type, 0, comm);
-    MPI_Scatter(a, local_n, type, local_vector, local_n, type, 0, comm);
+    MPI_Scatter(a, local_n, TYPE, local_matrix, local_n, TYPE, 0, comm);
+    MPI_Scatter(b, local_n, TYPE, local_vector, local_n, TYPE, 0, comm);
   }
 }  /* Read_data */
 
@@ -149,14 +164,10 @@ void Print_vector(double local_vec[], int local_n, int n, char title[],
                   int my_rank, MPI_Comm comm) {
   double *a = NULL;
   int i;
-  MPI_Datatype type;
-  
-  MPI_Type_contiguous(1, MPI_DOUBLE, &type);
-  MPI_Type_commit(&type);
   
   if (my_rank == 0) {
     a = malloc(n * sizeof(double));
-    MPI_Gather(local_vec, local_n, type, a, local_n, type, 0, comm);
+    MPI_Gather(local_vec, local_n, TYPE, a, local_n, TYPE, 0, comm);
     printf("%s\n", title);
     for (i = 0; i < n; i++) {
       printf("%.2f ", a[i]);
@@ -164,17 +175,47 @@ void Print_vector(double local_vec[], int local_n, int n, char title[],
     printf("\n");
     free(a);
   } else {
-    MPI_Gather(local_vec, local_n, type, a, local_n, type, 0, comm);
+    MPI_Gather(local_vec, local_n, TYPE, a, local_n, TYPE, 0, comm);
   }
   
 }  /* Print_vector */
 
 /*-------------------------------------------------------------------*/
-void Par_vector_addition(const double *local_vec1, const double *local_vec2, double *local_result, int local_n) {
+void Print_vector_2d(double local_vec[], int local_n, int n, char title[],
+                     int my_rank, MPI_Comm comm) {
+  double **a = NULL;
+  int i;
+  int j;
   
-  for (int local_i = 0; local_i < local_n; local_i++) {
-    local_result[local_i] = local_vec1[local_i] + local_vec2[local_i];
+  if (my_rank == 0) {
+    a = malloc(n * sizeof(double));
+    for (i = 0; i < pow(n, 2); i++) {
+      a[i] = (double *) malloc(n * sizeof(double));
+    }
+    MPI_Gather(local_vec, local_n, TYPE, a, local_n, TYPE, 0, comm);
+    printf("%s\n", title);
+    for (i = 0; i < n; i++) {
+      for (j = 0; j < n; ++j) {
+        printf("%.2f ", *&a[i][j]);
+      }
+    }
+    printf("\n");
+    free(a);
+  } else {
+    MPI_Gather(local_vec, local_n, TYPE, a, local_n, TYPE, 0, comm);
   }
   
+}  /* Print_vector */
+
+/*-------------------------------------------------------------------*/
+void
+Mat_vect_mult(const double *local_a, const double *local_x, double *local_result, int local_m, int n, int local_n) {
+  int local_i;
+  int i;
   
-}  /* Par_vector_addition */
+  for (local_i = 0; local_i < local_m; local_i++) {
+    for (i = 0; i < n; i++) {
+      local_result[local_i] += local_a[local_i * n + i] * local_x[i];
+    }
+  }
+}
